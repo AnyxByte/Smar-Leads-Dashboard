a0c50307d2e0bc0ac661a2ba98607afda06fb0ba
Anonymous Hacker <suprodipbhattacharjee1@gmail.com>
2026-05-18 21:22:17 +0530

    removed the unused variable and made it less strict

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Dashboard from "./pages/Dashboard.tsx";
-// @ts-expect-error: side-effect import for CSS
 import "./index.css";
 import Landing from "./pages/Landing.tsx";
 import Auth from "./pages/Auth.tsx";
